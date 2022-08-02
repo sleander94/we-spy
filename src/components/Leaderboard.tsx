732f3d304469a5cfe4e042922790b1676c5983de
Stephen Leander <stephen.leander1@gmail.com>
2022-08-02 09:58:08 -0600

    Add Leaderboard to sort & display scores

---
+++ src/components/Leaderboard.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { LeaderboardProps, Score } from '../types.d';
+
+const Leaderboard = ({ scores }: LeaderboardProps) => {
+  const [sortedScores, setSortedScores] = useState<Array<Score>>();
+
+  // Sort scores by time is ascending order
+  useEffect(() => {
+    const compareTime = (a: Score, b: Score) => {
+      if (a.time < b.time) {
+        return -1;
+      }
+      if (a.time > b.time) {
+        return 1;
+      }
+      return 0;
+    };
+    setSortedScores(scores.sort(compareTime));
+  }, [scores]);
+
+  return (
+    <section id="leaderboard">
+      <div className="headers">
+        <h2>Rank</h2>
+        <h2>Name</h2>
+        <h2>Time</h2>
+      </div>
+      {sortedScores?.map((score) => {
+        return (
+          <div className="score" key={score.id}>
+            <p>{scores.indexOf(score) + 1}</p>
+            <p>{score.user}</p>
+            <p>{score.time}</p>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Leaderboard;
