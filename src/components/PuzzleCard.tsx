59bb6984fb5e59040793dbac48557966eed70c78
Stephen Leander <stephen.leander1@gmail.com>
2022-08-02 20:12:07 -0600

    Replace 'a' elements with React Link components

--- src/components/PuzzleCard.tsx
+++ src/components/PuzzleCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { PuzzleCardProps } from '../types.d';
 
 const PuzzleCard = ({
@@ -9,12 +10,15 @@ const PuzzleCard = ({
 }: PuzzleCardProps) => {
   return (
     <div className="puzzle">
-      <a href={`/puzzles/${id}`}>{title}</a>
-      <a href={`/puzzles/${id}`}>
+      <Link className="title" to={`/puzzles/${id}`}>
+        {title}
+      </Link>
+      <a className="thumbnail" href={`/puzzles/${id}`}>
         <img src={image} alt="puzzle thumbnail" />
       </a>
       <div className="author-date">
-        <p>{author}</p>
+        <p>{author.split(' ')[0]}</p>
+        <Link to={`/leaderboards/${id}`}>View Leaderboard</Link>
         <p>{timestamp?.toDate().toLocaleDateString()}</p>
       </div>
     </div>
