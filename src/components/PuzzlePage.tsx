9c438a89e9f5d48bc500322d32f322e97bb01927
Stephen Leander <stephen.leander1@gmail.com>
2022-08-02 20:14:14 -0600

    Handle image load & canvas resize with state, render ScoreUploader on game over

--- src/components/PuzzlePage.tsx
+++ src/components/PuzzlePage.tsx
@@ -1,26 +1,64 @@
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, SyntheticEvent } from 'react';
 import { doc, getDoc } from 'firebase/firestore/lite';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 import { db } from '../firebase/client';
-import { HiddenItem, Puzzle } from '../types.d';
+import { HiddenItem, Puzzle, FormProps } from '../types.d';
+import ScoreUploader from './ScoreUploader';
 
-const PuzzlePage = () => {
+const PuzzlePage = ({ username, userId }: FormProps) => {
   const { id } = useParams<string>();
-  const [puzzle, setPuzzle] = useState<Puzzle>({
-    author: '',
-    authorId: '',
-    title: '',
-    image: '',
-    hiddenItems: [],
-  });
+  const [puzzle, setPuzzle] = useState<Puzzle>();
   const [hiddenItems, setHiddenItems] = useState<Array<HiddenItem>>([]);
   const [activeItem, setActiveItem] = useState<string>('');
-  const [timer, setTimer] = useState<number>(0);
+
+  // Get puzzle from firestore & image URL from storage before setting state
+
+  const [imageSrc, setImageSrc] = useState<string>('');
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
 
   const storage = getStorage();
 
-  // Start a timer after getting puzzle info, stop when all items are found
+  useEffect(() => {
+    const getPuzzle = async () => {
+      try {
+        const results = await getDoc(doc(db, 'puzzles', `${id}`));
+        let data = results.data() as Puzzle;
+        const url = await getDownloadURL(ref(storage, data.image));
+        data.image = url;
+
+        setPuzzle(data);
+        setHiddenItems(data.hiddenItems);
+        setImageSrc(url);
+        setImageLoaded(true);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getPuzzle();
+  }, []);
+
+  const [canvasWidth, setCanvasWidth] = useState<number>(0);
+  const [canvasHeight, setCanvasHeight] = useState<number>(0);
+
+  const scaleCanvas = (e: SyntheticEvent) => {
+    const puzzleImage = e.target as HTMLImageElement;
+    setCanvasWidth(puzzleImage.width);
+    setCanvasHeight(puzzleImage.height);
+    setTimer(0.1);
+    setGameStart(true);
+  };
+
+  window.onresize = () => {
+    const puzzleImage = document.getElementById('image') as HTMLImageElement;
+    setCanvasWidth(puzzleImage.width);
+    setCanvasHeight(puzzleImage.height);
+  };
+
+  // Start game & timer after getting hidden items from puzzle, stop when all items are found
+  const [timer, setTimer] = useState<number>(0);
+  const [gameStart, setGameStart] = useState<boolean>(false);
+
   useEffect(() => {
     if (hiddenItems.length > 0) {
       const count = setInterval(
@@ -31,50 +69,9 @@ const PuzzlePage = () => {
     }
   }, [timer]);
 
-  // Get puzzle from firestore & image URL from storage before setting state
-  useEffect(() => {
-    const getPuzzle = async () => {
-      try {
-        const results = await getDoc(doc(db, 'puzzles', `${id}`));
-        let data = results.data() as Puzzle;
-        const url = await getDownloadURL(ref(storage, data.image));
-        data.image = url;
-        setPuzzle(data);
-        setHiddenItems(data.hiddenItems);
-
-        // Create & append image with correct source
-        const puzzleImage = document.createElement('img') as HTMLImageElement;
-        puzzleImage.id = 'puzzle-image';
-        puzzleImage.alt = 'puzzle image';
-        puzzleImage.src = url;
-        const gameImage = document.getElementById('game-image');
-        gameImage?.appendChild(puzzleImage);
-
-        // After image is loaded, get width & height & adjust canvas
-        puzzleImage.addEventListener('load', () => {
-          const width = puzzleImage.width;
-          const height = puzzleImage.height;
-          const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-          canvas.style.width = `${width}px`;
-          canvas.style.height = `${height}px`;
-
-          // Initialize timer after image is loaded
-          setTimer(0.1);
-        });
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getPuzzle();
-
-    // Add class to Navbar to adjust left margin
-    const nav = document.getElementById('nav');
-    nav?.classList.add('game-nav');
-  }, []);
-
   // Set the first hidden item to active after puzzle is loaded
   useEffect(() => {
-    if (puzzle.hiddenItems.length > 0) {
+    if (puzzle && puzzle.hiddenItems.length > 0) {
       const description = puzzle.hiddenItems[0].description;
       const firstItem = document.getElementById(description);
       firstItem?.classList.add('active');
@@ -134,11 +131,14 @@ const PuzzlePage = () => {
 
   return (
     <section id="puzzle-page">
+      {hiddenItems.length == 0 && gameStart && (
+        <ScoreUploader username={username} timer={Number(timer)} />
+      )}
       <div className="gameboard">
         <div className="game-text">
-          <h2>{puzzle.title}</h2>
+          <h2>{puzzle?.title}</h2>
           <ol>
-            {puzzle.hiddenItems.map((item: HiddenItem) => {
+            {puzzle?.hiddenItems.map((item: HiddenItem) => {
               return (
                 <li
                   id={item.description}
@@ -153,7 +153,15 @@ const PuzzlePage = () => {
           <p className="timer">{timer}</p>
         </div>
         <div id="game-image" className="game-image">
-          <canvas id="canvas" onClick={handleGuess(activeItem)}></canvas>
+          {imageLoaded && (
+            <img id="image" src={imageSrc} alt="" onLoad={scaleCanvas} />
+          )}
+          <canvas
+            id="canvas"
+            width={canvasWidth}
+            height={canvasHeight}
+            onClick={handleGuess(activeItem)}
+          ></canvas>
         </div>
       </div>
     </section>
