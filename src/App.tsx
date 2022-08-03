b925ab9ac760dd910e2581c16ec3aa77607eb127
Stephen Leander <stephen.leander1@gmail.com>
2022-08-02 20:16:28 -0600

    Setup Routes, remove boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,60 @@
-import React from 'react';
-import Counter from './components/Counter';
-import './App.css';
-// import Login from './components/login/Login'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { auth } from './firebase/client';
+import { onAuthStateChanged } from 'firebase/auth';
+import PuzzleForm from './components/PuzzleForm';
+import Puzzles from './components/Puzzles';
+import PuzzlePage from './components/PuzzlePage';
+import Navbar from './components/Navbar';
+import LeaderboardPage from './components/LeaderboardPage';
+import './styles/main.css';
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState<string>('');
+  const [userId, setUserId] = useState<string>('');
+  const [loggedIn, setLoggedIn] = useState<boolean>(true);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      if (user.displayName) setUsername(user.displayName);
+      if (user.uid) setUserId(user.uid);
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Counter />
-      </header>
-      {/* <Login /> */}
+      <Navbar loggedIn={loggedIn} username={username} />
+      <Router>
+        <Routes>
+          <Route path="/puzzles" element={<Puzzles />} />
+          <Route
+            path="/puzzles/:id"
+            element={
+              <PuzzlePage
+                loggedIn={loggedIn}
+                username={username}
+                userId={userId}
+              />
+            }
+          />
+          <Route path="/leaderboards/:id" element={<LeaderboardPage />} />
+          <Route
+            path="/puzzles/new"
+            element={
+              <PuzzleForm
+                loggedIn={loggedIn}
+                username={username}
+                userId={userId}
+              />
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
