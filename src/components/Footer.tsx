dc1b461d7ef5771a100f8567266e409386e8fa48
Stephen Leander <stephen.leander1@gmail.com>
2022-08-06 12:17:15 -0600

    Initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <section id="footer">
+      <img src={require('../assets/icons/android-chrome-192x192.png')} alt="" />
+      <p>We Spy - 2022</p>
+      <a href="https://github.com/sleander94" target="_blank">
+        <img
+          src={require('../assets/icons/iconmonstr-github-1.svg').default}
+          alt=""
+        />
+      </a>
+    </section>
+  );
+};
+
+export default Footer;
