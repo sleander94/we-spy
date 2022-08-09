9d9886222c84a531c8652ad568e6fcfd3f073d2f
Stephen Leander <stephen.leander1@gmail.com>
2022-08-09 13:27:06 -0600

    Initial commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <section id="home">
+      <h1>Welcome to We Spy</h1>
+      <h2>How to Play</h2>
+      <div className="left-instruction">
+        <img src={require('../assets/images/all-puzzles.png')} alt="" />
+        <p>1. Select a puzzle to start playing.</p>
+      </div>
+      <div className="right-instruction">
+        <p>2. Find all the hidden items to complete the puzzle.</p>
+        <img src={require('../assets/images/puzzle-page.png')} alt="" />
+      </div>
+      <div className="left-instruction">
+        <img src={require('../assets/images/selector-demo.gif')} alt="" />
+        <p>3. Click on an item and select it from the list.</p>
+      </div>
+      <div className="right-instruction">
+        <p>4. Find all the items and share your score!</p>
+        <img src={require('../assets/images/leaderboard-page.png')} alt="" />
+      </div>
+      <Link to={'/puzzles'}>View Puzzles</Link>
+    </section>
+  );
+};
+
+export default Home;
