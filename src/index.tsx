3ce9160f49f6ff8e8c9006d1baa077bf68ab020b
Stephen Leander <stephen.leander1@gmail.com>
2022-08-16 08:51:39 -0600

    Remove store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -10,8 +7,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
