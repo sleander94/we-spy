dadf7ad1aac1b947e534e59218b457e80e2fb065
Stephen Leander <stephen.leander1@gmail.com>
2022-08-31 17:21:33 -0600

    Paginate puzzle data & add next / prev buttons

--- src/components/Puzzles.tsx
+++ src/components/Puzzles.tsx
@@ -1,5 +1,15 @@
 import { useState, useEffect } from 'react';
-import { collection, getDocs, query, orderBy } from 'firebase/firestore/lite';
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  startAfter,
+  limit,
+  DocumentData,
+  QueryDocumentSnapshot,
+  OrderByDirection,
+} from 'firebase/firestore/lite';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 import { db } from '../firebase/client';
 import { Puzzle } from '../types.d';
@@ -8,45 +18,40 @@ import PuzzleCard from './PuzzleCard';
 const Puzzles = () => {
   const [puzzles, setPuzzles] = useState<Puzzle[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [sortMethod, setSortMethod] = useState<string>('views');
+  const [firstPuzzle, setFirstPuzzle] =
+    useState<QueryDocumentSnapshot<DocumentData> | null>(null);
+  const [lastPuzzle, setLastPuzzle] =
+    useState<QueryDocumentSnapshot<DocumentData> | null>(null);
 
   const storage = getStorage();
 
   // Get all puzzles from firestore & their image URLS from storage before setting state
-  useEffect(() => {
-    const getPuzzles = async () => {
-      try {
-        const results = await getDocs(
-          query(collection(db, 'puzzles'), orderBy('views', 'desc'))
-        );
-        let data: Puzzle[] = [];
-        results.forEach((puzzle) => {
-          let info = puzzle.data();
-          info.id = puzzle.id;
-          data.push(info as Puzzle);
-        });
-        for (const puzzle of data) {
-          const url = await getDownloadURL(
-            ref(storage, `puzzles/thumb_${puzzle.image}`)
-          );
-          puzzle.image = url;
-        }
-        setPuzzles(data);
-        setLoading(false);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getPuzzles();
-  }, []);
-
-  const sortPuzzles = async (order: string) => {
+  const getPuzzles = async (
+    dir: OrderByDirection,
+    startIndex: QueryDocumentSnapshot<DocumentData> | null = null
+  ) => {
+    setLoading(true);
     try {
-      setLoading(true);
-      const results = await getDocs(
-        query(collection(db, 'puzzles'), orderBy(order, 'desc'))
-      );
+      let results;
+      if (startIndex) {
+        results = await getDocs(
+          query(
+            collection(db, 'puzzles'),
+            orderBy(sortMethod, dir),
+            startAfter(startIndex),
+            limit(8)
+          )
+        );
+      } else {
+        results = await getDocs(
+          query(collection(db, 'puzzles'), orderBy(sortMethod, dir), limit(8))
+        );
+      }
+      let docs = results.docs;
+      if (dir === 'asc') docs = docs.reverse();
       let data: Puzzle[] = [];
-      results.forEach((puzzle) => {
+      docs.forEach((puzzle) => {
         let info = puzzle.data();
         info.id = puzzle.id;
         data.push(info as Puzzle);
@@ -58,14 +63,21 @@ const Puzzles = () => {
         puzzle.image = url;
       }
       setPuzzles(data);
+      setFirstPuzzle(docs[0]);
+      setLastPuzzle(docs[docs.length - 1]);
       setLoading(false);
     } catch (err) {
       console.error(err);
     }
   };
 
+  useEffect(() => {
+    getPuzzles('desc');
+  }, [sortMethod]);
+
   return (
     <section id="puzzles">
+      <h1>Puzzles</h1>
       {loading && (
         <div className="loading-container">
           <div className="rect1"></div>
@@ -75,10 +87,19 @@ const Puzzles = () => {
       )}
       {!loading && (
         <div id="puzzle-container">
-          <h1>Puzzles</h1>
           <div className="sort-buttons">
-            <button onClick={() => sortPuzzles('views')}>Most Viewed</button>
-            <button onClick={() => sortPuzzles('timestamp')}>Recent</button>
+            <button
+              className={sortMethod === 'views' ? 'active' : ''}
+              onClick={() => setSortMethod('views')}
+            >
+              Top
+            </button>
+            <button
+              className={sortMethod === 'timestamp' ? 'active' : ''}
+              onClick={() => setSortMethod('timestamp')}
+            >
+              Recent
+            </button>
           </div>
           <div className="puzzle-grid">
             {puzzles.map((puzzle: Puzzle) => {
@@ -95,6 +116,14 @@ const Puzzles = () => {
                 />
               );
             })}
+          </div>
+          <div className="page-container">
+            <button onClick={() => getPuzzles('asc', firstPuzzle)}>
+              {'<'} prev
+            </button>
+            <button onClick={() => getPuzzles('desc', lastPuzzle)}>
+              next {'>'}
+            </button>
           </div>
         </div>
       )}
